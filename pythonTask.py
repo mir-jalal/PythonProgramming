898715d7f46b6fd9bb39e7af47a34f16de28f3f4
Mir Jalal <35169094+mircelal99@users.noreply.github.com>
2020-07-17 01:21:20 +0400

    Update pythonTask.py

--- pythonTask.py
+++ pythonTask.py
@@ -1,15 +1,15 @@
-prime = 1000000*[False]
+prime = 1000000*[True]
 
 for i in range(2, 1000):
     for j in range(2, 1000):
-        prime[i*j] = True;
+        prime[i*j] = False;
 
 sum = 0
 
 k = int(input(k))
 
 for i in range(2, 1000000):
-    if not prime[i]:
+    if prime[i]:
         sum = sum + i
         if sum % k == 0:
             break
